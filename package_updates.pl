d4d3eba672e689edb1be1336351743c0502b2244
Jochen Maes <github@gcool.be>
2014-05-20 15:03:11 +0200

    package update script

--- package_updates.pl
+++ package_updates.pl
@@ -1,16 +1,5 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-my $n = ((`pacman -Qu` =~ m/^[^\s]+\s\((\d+)\):/m) ? $1 : 0);
-if ($n == 0)
-{
-     print "System is up to date"
-}
-elsif($n == 1)
-{
-     print "1 new package available"
-}
-else
-{
-     print "$n new packages available"
-}
+my $n = ((`pacman -Qu | wc -l`));
+print $n;
